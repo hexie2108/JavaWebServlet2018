31cf4ffabb6b8bff6c938f5706f75269e65c85ed
Enrico Soprana <esoprana@gmail.com>
2018-07-09 15:46:00 +0200

    Add session error field for email

--- src/main/java/it/unitn/webprogramming18/dellmm/servlets/userSystem/RegisterServlet.java
+++ src/main/java/it/unitn/webprogramming18/dellmm/servlets/userSystem/RegisterServlet.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.HashMap;
+import java.util.List;
 
 @WebServlet(name = "RegisterServlet")
 public class RegisterServlet extends HttpServlet {
@@ -78,12 +79,19 @@ public class RegisterServlet extends HttpServlet {
                 HttpSession session = request.getSession();
                 session.setAttribute("user", user);
 
-                emailFactory.sendMail(
-                    "Registration",
-                    "Registration",
-                    VerifyLinkMail.createMessage(user),
-                    "registrazioneprogettowebprog@gmail.com"
-                ); // Per ora le mandiamo a noi stessi per evitare casini
+
+                try {
+                    emailFactory.sendMail(
+                            "Registration",
+                            "Registration",
+                            VerifyLinkMail.createMessage(user),
+                            "registrazioneprogettowebprog@gmail.com"
+                    ); // Per ora le mandiamo a noi stessi per evitare casini
+                }
+                catch (MessagingException | UnsupportedEncodingException ex) {
+                    List errorList = (List<String>)session.getAttribute("errors");
+                    errorList.add("Impossible to send the email. Please check the email in user's settings and click resend");
+                }
             } catch (DAOException e) {
                 if(e.getCause() instanceof SQLIntegrityConstraintViolationException) {
                     messages.put("Email","Email già utilizzata");
@@ -93,11 +101,6 @@ public class RegisterServlet extends HttpServlet {
                 } else {
                     response.sendError(500, "Impossible register the user. The server returned: " + e.getMessage());
                 }
-            } catch (MessagingException | UnsupportedEncodingException ex) {
-                response.sendError(
-                        500,
-                        "Impossible to send the email. The server returned: " + ex.getMessage() +". The user is already logged in."
-                );
             }
         } else {
             request.setAttribute("messages",messages);
