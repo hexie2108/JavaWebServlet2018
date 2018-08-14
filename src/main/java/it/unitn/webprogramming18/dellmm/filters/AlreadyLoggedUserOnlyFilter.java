fd35539057b77f29c687f7a91510e9e2ec90d400
mikuc <mikuc@DESKTOP-T4154A4>
2018-08-14 17:36:21 +0200

    add alreadyUserFilter

---
+++ src/main/java/it/unitn/webprogramming18/dellmm/filters/AlreadyLoggedUserOnlyFilter.java
@@ -0,0 +1,60 @@
+package it.unitn.webprogramming18.dellmm.filters;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * se utente è loggato e sta navigando le pagine di userSystem dedicato per user
+ * non loggato
+ *
+ */
+public class AlreadyLoggedUserOnlyFilter implements Filter
+{
+
+        @Override
+        public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException
+        {
+
+                HttpServletRequest request = (HttpServletRequest) req;
+                HttpServletResponse response = (HttpServletResponse) resp;
+
+                // Non fare caching
+                response.setHeader("Cache-Control", "no-cache");
+                response.setHeader("Pragma", "no-cache");
+                response.setDateHeader("Expires", -1);
+                response.setCharacterEncoding("UTF-8");
+
+                //get la sessione
+                HttpSession session = request.getSession(false);
+
+                // Se esiste la sessione oppure l'utente  è autenticato
+                if (session != null && session.getAttribute("user") != null)
+                {
+                        //get il percorso base
+                        String contextPath = request.getServletContext().getContextPath();
+                        //indirizza a home
+                        response.sendRedirect(contextPath);
+                }
+                else
+                {
+
+                        // Se l'utente non è  autenticato facciamo continuare
+                        chain.doFilter(req, resp);
+                }
+
+        }
+
+        @Override
+        public void init(FilterConfig config) throws ServletException
+        {
+        }
+
+        @Override
+        public void destroy()
+        {
+        }
+}
--- src/main/java/it/unitn/webprogramming18/dellmm/servlets/userSystem/LoginServlet.java
+++ src/main/java/it/unitn/webprogramming18/dellmm/servlets/userSystem/LoginServlet.java
@@ -22,44 +22,34 @@ public class LoginServlet extends HttpServlet
         {
                 String prevUrl = null;
 
-                //se utente non è ancora loggato
-                if (request.getSession().getAttribute("user") == null)
+                prevUrl = request.getParameter(FormValidator.PREV_URL_KEY);
+                //se non c'è preUrl dal parametro, set preUrl come URL di provenienza
+                if (prevUrl == null)
                 {
-
-                        prevUrl = request.getParameter(FormValidator.PREV_URL_KEY);
-                        //se non c'è preUrl dal parametro, set preUrl come URL di provenienza
-                        if (prevUrl == null)
+                        prevUrl = request.getHeader("Referer");
+                        //se URL di provenienza sono le pagine di user system, ignorarlo
+                        for (int i = 0; i < FormValidator.pageNameOfUserSystem.length; i++)
                         {
-                                prevUrl = request.getHeader("Referer");
-                                //se URL di provenienza sono le pagine di user system, ignorarlo
-                               for (int i = 0; i < FormValidator.pageNameOfUserSystem.length; i++)
+                                if (prevUrl != null && prevUrl.contains(FormValidator.pageNameOfUserSystem[i]))
                                 {
-                                        if (prevUrl != null && prevUrl.contains(FormValidator.pageNameOfUserSystem[i]))
-                                        {
-                                                prevUrl = null;
-                                                i=99;
-                                        }
+                                        prevUrl = null;
+                                        i = 99;
                                 }
                         }
-
-                        //se è nullo, set la provenienza come string vuota
-                        if (prevUrl == null)
-                        {
-                                prevUrl = "";
-                        }
-
-                        //set il titolo della pagina
-                        request.setAttribute(ConstantsUtils.HEAD_TITLE, "login");
-                        request.setAttribute(FormValidator.PREV_URL_KEY, prevUrl);
-                        request.getRequestDispatcher(JSP_PAGE_PATH).forward(request, response);
                 }
 
-                //se utente è già loggato
-                else
+                //se è nullo, set la provenienza come string vuota
+                if (prevUrl == null)
                 {
-                        prevUrl = getServletContext().getContextPath();
-                        response.sendRedirect(response.encodeRedirectURL(prevUrl));
+                        prevUrl = "";
                 }
+
+                //set il titolo della pagina
+                request.setAttribute(ConstantsUtils.HEAD_TITLE, "login");
+                request.setAttribute(FormValidator.PREV_URL_KEY, prevUrl);
+                request.getRequestDispatcher(JSP_PAGE_PATH).forward(request, response);
         }
+
+}
 
 }
